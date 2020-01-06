83187525acab44092bcffe52327e3391cde6af10
曾鸿发 <1594384725@qq.com>
2020-01-06 15:38:54 +0800

    init project

---
+++ src/main/java/com/zhf/Test.java
@@ -0,0 +1,36 @@
+package com.zhf;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created on 2020/1/6 0006.
+ */
+public class Test {
+
+    public void generator() throws Exception{
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("src/main/resources/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Test t = new Test();
+            t.generator();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
